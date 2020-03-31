1d17ad34e0fd626594379e6b50332cfd836b70a4
Lauren Birts <birtslauren@gmail.com>
2020-03-31 18:37:20 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -11,15 +11,3 @@ def run_guessing_game
     puts "Sorry! The computer guessed #{random_num}."
   end
 end
-
-# def run_guessing_game 
-#   secret_number = rand(6)+1
-#   puts "Guess a my number between 1 and 6!"
-#   user_guess = gets.chomp
-#   if user_guess.to_i == secret_number
-#     puts "You guessed the correct number!"
-#   elsif user_guess == "exit"
-#     puts "Goodbye!"
-#   end 
-#   puts "Sorry! The computer guessed #{secret_number}."
-# end 

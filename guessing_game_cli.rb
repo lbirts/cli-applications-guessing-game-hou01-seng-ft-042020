3e7ed5bf64f822835bc6800604274f5884584b25
Lauren Birts <birtslauren@gmail.com>
2020-03-31 18:09:23 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
-  end
+  else
     puts "Sorry! The computer guessed #{random_num}"
+  end
 end
